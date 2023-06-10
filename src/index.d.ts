9082c396c7289be793eb93eb917b28201a4c00f7
Mykhailo Unniiiverse <unniiiverse@gmail.com>
2023-06-10 22:40:13 +0300

    1.4.11/migration patch

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,7 @@
-/* eslint-disable no-unused-vars */
-
-import type { Burger, IParams as IBurgerParams } from './src/API/burger.js';
-import type { Popup, IParams as IPopupParams } from './src/API/popup.js';
-import type { Accordion, IParams as IAccordionParams } from './src/API/accordion.js';
-import type { BlImage, IProps as IBlImageProps } from './src/API/blimage.js';
-import type { FormValidator, IFormInputRules, IFormValidatorParams, TThrow } from './src/API/formValidator.js';
-
-export { Burger, Popup, Accordion, BlImage, FormValidator, IFormInputRules };
+export * from '../dist/functions.js';
+export { Burger } from '../dist/burger.js';
+export { Accordion } from '../dist/accordion.js';
+export { Popup } from '../dist/popup.js';
+export { BlImage } from '../dist/blimage.js';
+export { FormValidator } from '../dist/formValidator.js';
+export type { IFormInputRules } from '../dist/formValidator.js';
---
+++ src/src/API/index.ts
@@ -0,0 +1,7 @@
+export * from './functions.js';
+export { Burger } from './burger.js';
+export { Accordion } from './accordion.js';
+export { Popup } from './popup.js';
+export { BlImage } from './blimage.js';
+export { FormValidator } from './formValidator.js';
+export type { IFormInputRules } from './formValidator.js';
